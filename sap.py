dbc4d55d9ccdd0f478f6f46ef2813932d2f50356
Camila Pozas <cpozasg1103@gmail.com>
2021-08-23 16:49:33 -0400

    consolidated soc

--- sap.py
+++ sap.py
@@ -7,9 +7,11 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import os
+import shutil
+import pandas as pd
 
-
-
+# Credenciales
 env = Env()
 env.read_env()  # read .env file, if it exists
 # required variables
@@ -17,8 +19,15 @@ user_name = env("user_name")
 password = env("password") 
 driver_path = env("driver_path",'chromedriver.exe')  
 
+output_dir = os.path.join(os.getcwd(), 'output')
+options = webdriver.ChromeOptions() 
+download_argument = f'download.default_directory={output_dir}'
+prefs = {'download.default_directory' : output_dir}
+options.add_experimental_option('prefs', prefs)
+
+# Ingreso a transacción y descarga
 def descarga(soc):
-    driver = webdriver.Chrome(driver_path) # Abrimos en Chrome
+    driver = webdriver.Chrome(driver_path, options = options)
 
     #   Ingresar a SAP
     driver.get("https://dims4prdci.dimerc.cl:8001/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html?sap-client=300&sap-language=ES#Customer-manageLineItems?sap-ui-tech-hint=GUI")
@@ -36,8 +45,10 @@ def descarga(soc):
     # Llenar datos
     element = driver.find_element_by_id("M0:46:::2:34") # Sociedad
     element.send_keys(soc)
-    #element = driver.find_element_by_id("M0:46:::2:59")
-    #element.send_keys(soc2)
+    layout = "/AUTOMATI"    # Layout
+    element = driver.find_element_by_id("M0:46:::30:34")
+    element.clear()
+    element.send_keys(layout)
     ayer = datetime.today() - timedelta(days=1)
     d1 = ayer.strftime("%d.%m.%Y")
     element = driver.find_element_by_id("M0:46:::12:34")    # Fecha
@@ -94,9 +105,44 @@ def descarga(soc):
     finally:
         driver.quit()
 
+def limpiar_output(output_dir):
+  # Si existe la carpeta la eliminamos
+  if os.path.isdir(output_dir):
+    shutil.rmtree(output_dir)
+  # Si existe un archivo con el mismo nombre que la carpeta
+  # lo eliminamos
+  if os.path.isfile(output_dir):
+    os.remove(output_dir)
+  # Creamos la carpeta para que este limpia
+  os.mkdir(output_dir)
 
-#   Extracción para todas las sociedades
-sociedades = [2000,2100,2200,3000,3100]
+
+def consolidar(output_dir):
+  df = pd.DataFrame()
+  # Filtramos solo los archivos excel
+  excels = [file for file in os.listdir(output_dir) if file.endswith('.xlsx')]
+  for excel in excels:
+    
+    print(os.path.join(output_dir, excel))
+    # Leemos archivo
+    tmp = pd.read_excel(os.path.join(output_dir, excel))
+    print(tmp)
+
+    if df.empty:
+      df = tmp.copy()
+    else:
+      df = pd.concat([df, tmp])
+  print(df)
+  return df
+
+# Limpiamos output
+limpiar_output(output_dir)
+# Corremos for
+sociedades = [2100,3100]
 
 for i in sociedades:
     descarga(i)
+
+# Consolidamos en un solo archivo
+consolidado = consolidar(output_dir)
+consolidado.to_excel('consolidado.xlsx')
