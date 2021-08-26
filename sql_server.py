cfa2c9779e392f0aa51636aa42b8d10e5dcff89a
Camila Pozas <cpozasg1103@gmail.com>
2021-08-26 13:28:11 -0400

    update sql ("functional")

---
+++ sql_server.py
@@ -0,0 +1,19 @@
+import pyodbc
+from sqlalchemy import create_engine
+import pandas as pd
+from params import server_sql, database, user_sql, pass_sql, mssql_driver
+
+# Establecemos conexión con SQL Server
+def connection():
+    engine = create_engine(f'mssql+pyodbc://{user_sql}:{pass_sql}@{server_sql}/{database}?driver={mssql_driver}', 
+            fast_executemany = True)
+    return engine
+
+# Probamos conexión a SQL Server y la respectiva tabla, si existe reemplazamos los valores.
+def test(df):
+    engine = connection()
+    df.to_sql('data_diaria_fbl5n', con=engine, if_exists='replace', index=False)
+
+# Leemos df
+df = pd.read_excel('consolidado.xlsx')
+test(df)    # Guardamos valores en SQL
